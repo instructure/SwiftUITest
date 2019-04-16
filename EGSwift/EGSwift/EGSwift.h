87b9f002fe12555dba5689ef8bfb2276572a6278
bootstraponline <matt@bootstraponline.com>
2019-04-16 15:41:36 -0400

    Add EGSwift.xcodeproj

---
+++ EGSwift/EGSwift/EGSwift.h
@@ -0,0 +1,19 @@
+//
+//  EGSwift.h
+//  EGSwift
+//
+//  Created by Matthew Edwards on 4/16/19.
+//  Copyright © 2019 instructure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EGSwift.
+FOUNDATION_EXPORT double EGSwiftVersionNumber;
+
+//! Project version string for EGSwift.
+FOUNDATION_EXPORT const unsigned char EGSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EGSwift/PublicHeader.h>
+
+
