aa610d96dc3b451afe071b50b26cf341bb875535
bootstraponline <matt@bootstraponline.com>
2019-05-09 14:07:16 -0400

    Add ElementWrapper

--- SwiftUITest/ElementId.swift
+++ SwiftUITest/ElementId.swift
@@ -1,4 +1,5 @@
 import Foundation
+import XCTest
 
 // Enable defining enums that have a string element id
 //
@@ -6,12 +7,83 @@ import Foundation
 //      case resetPassword
 //  }
 
-public protocol ElementId where Self: RawRepresentable, Self.RawValue: StringProtocol {
+public protocol ElementId {
     var id: String { get }
 }
 
-public extension ElementId {
+// Provide default 'id' implementation for enums
+public extension ElementId where Self: RawRepresentable, Self.RawValue: StringProtocol {
     var id: String {
         return "\(String(describing: Self.self)).\(rawValue)"
+    }
+}
+
+public protocol ElementWrapper : Element {
+    var element: Element { get }
+}
+
+public extension ElementWrapper {
+    var exists: Bool {
+        return element.exists
+    }
+
+    var label: String {
+        return element.label
+    }
+
+    var id: String {
+        return element.id
+    }
+
+    var elementType: XCUIElement.ElementType {
+        return element.elementType
+    }
+
+    var isVisible: Bool {
+        return element.isVisible
+    }
+
+    var isVisibleNow: Bool {
+        return element.isVisibleNow
+    }
+
+    var value: String {
+        return element.value
+    }
+
+    var isEnabled: Bool {
+        return element.isEnabled
+    }
+
+    func tap() {
+        element.tap()
+    }
+
+    func typeText(_ text: String) {
+        element.typeText(text)
+    }
+
+    func swipeDown() {
+        element.swipeDown()
+    }
+
+    func swipeUp() {
+        element.swipeUp()
+    }
+
+    func child(label: String) -> Element {
+        return element.child(label: label)
+    }
+
+    func child(elementType: XCUIElement.ElementType, index: Int) -> Element {
+        return element.child(elementType: elementType, index: index)
+    }
+
+    func waitToExist(_ timeout: Timeout) -> Bool {
+        return element.waitToExist(timeout)
+    }
+
+    func waitToVanish(_ timeout: Timeout) {
+        return element.waitToVanish(timeout)
     }
 }
