2371aee63e27862b27598d2d2f7c741d407ceab6
bootstraponline <matt@bootstraponline.com>
2019-05-16 11:40:09 -0400

    Update XCElementType_Tests.swift

--- Tests/XCElementType_Tests.swift
+++ Tests/XCElementType_Tests.swift
@@ -5,6 +5,7 @@ class XCElementType_Tests: SwiftUITestCase {
 
     func test_ElementType_to_string() {
         XCTAssertEqual(XCElementType.any, "any")
+        XCTAssertEqual(XCElementType.segmentedControl, "segmentedControl")
         XCTAssertEqual(XCElementType.other, "other")
     }
 
@@ -16,6 +17,10 @@ class XCElementType_Tests: SwiftUITestCase {
     func test_from_uint_to_string() {
         XCTAssertEqual(XCElementType.from(0), "any")
         XCTAssertEqual(XCElementType.from(1), "other")
+
+        XCTAssertEqual(
+            XCElementType.from(XCUIElement.ElementType.segmentedControl.rawValue),
+            "segmentedControl")
     }
 
     func test_from_UISegment_to_ElementType() {
